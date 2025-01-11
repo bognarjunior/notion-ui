c53a5c25389d04fd6cabe05f2f4da4418f127e12
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-11 18:16:56 -0300

    Create routes
    Create routes

--- src/app/(tabs)/styles.ts
+++ src/app/(tabs)/styles.ts
@@ -1,8 +1,15 @@
 import { StyleSheet } from 'react-native';
+import { theme } from '@/theme';
+
+const { colors } = theme;
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: colors.White,
   }
 });
