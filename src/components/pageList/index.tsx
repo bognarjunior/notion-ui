d6b2315913cb0aa3e623c062bc28d3dc59476eb1
Bognar Junior <bognar_junior@yahoo.com.br>
2025-01-12 19:24:27 -0300

    Create components
    Create components Header, Page, PageList, Recent, RecentList

---
+++ src/components/pageList/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { styles } from './styles';
+
+export default function PageList() {
+  return (
+    <View>
+      <Text>PageList</Text>
+    </View>
+  )
+}
---
+++ src/components/recentList/styles.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '@/theme';
+
+const { colors } = theme;
+export const styles = StyleSheet.create({
+  container: {
+    
+  }
+});
